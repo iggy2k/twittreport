16b16dd8ae3ead675e78391233dacf22314149a6
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2022-04-30 16:49:31 -0400

    added dataclass to analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 from __future__ import annotations, unicode_literals, print_function
+from dataclasses import dataclass
 import spacy
 from spacy.lang.en import English
 
@@ -101,48 +102,107 @@ def aveSentWords(sentences):
     return words // len(sentences)
 
 
-print("Hang tight! Analyzing may take a while.")
+if __name__ == '__main__':
 
-# Load English tokenizer, tagger, parser and NER
-with open('clean.txt', encoding="utf8") as file:
-    full_text = file.read()
+    print("Hang tight! Analyzing may take a while.")
 
-# Boilerplate
-sentences = getSentences(full_text)
-nlp = spacy.load("en_core_web_sm")
-# Set max length to avoid exception
-doc = nlp(full_text[: 1000000])
+    # Load English tokenizer, tagger, parser and NER
+    with open('clean.txt', encoding="utf8") as file:
+        full_text = file.read()
+
+    # Boilerplate
+    sentences = getSentences(full_text)
+    nlp = spacy.load("en_core_web_sm")
+    # Set max length to avoid exception
+    doc = nlp(full_text[: 1000000])
 
 
-# Analyze syntax (boilerplate)
-noun_arr = [chunk.text for chunk in doc.noun_chunks]
-verb_arr = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+    # Analyze syntax (boilerplate)
+    noun_arr = [chunk.text for chunk in doc.noun_chunks]
+    verb_arr = [token.lemma_ for token in doc if token.pos_ == "VERB"]
 
-nouns = organizeNouns(noun_arr)
-verbs = organizeVerbs(verb_arr)
-structures = getStructures(sentences)
+    nouns = organizeNouns(noun_arr)
+    verbs = organizeVerbs(verb_arr)
+    structures = getStructures(sentences)
 
-out = open('analyzed.csv', 'w')
+    out = open('analyzed.csv', 'w')
 
-out.write("Total tweets,{}\n".format(
-    str(sum(1 for line in open('clean.txt', encoding="utf8")))))
+    out.write("Total tweets,{}\n".format(
+        str(sum(1 for line in open('clean.txt', encoding="utf8")))))
 
-out.write("Total sentences,{}\n".format(str(len(sentences))))
+    out.write("Total sentences,{}\n".format(str(len(sentences))))
 
-out.write("Average sentence length <chars> <words>,{},{}\n".format(
-    str(aveSentLen(sentences)), str(aveSentWords(sentences))))
+    out.write("Average sentence length <chars> <words>,{},{}\n".format(
+        str(aveSentLen(sentences)), str(aveSentWords(sentences))))
 
-out.write("Most used verbs\n")
-for top_verb in list(verbs)[:10]:
-    out.write("{},{}\n".format(
-        top_verb, verbs[top_verb]))
+    out.write("Most used verbs\n")
+    for top_verb in list(verbs)[:10]:
+        out.write("{},{}\n".format(
+            top_verb, verbs[top_verb]))
 
-out.write("Most used nouns\n")
-for top_noun in list(nouns)[:10]:
-    out.write("{},{}\n".format(
-        top_noun, nouns[top_noun]))
+    out.write("Most used nouns\n")
+    for top_noun in list(nouns)[:10]:
+        out.write("{},{}\n".format(
+            top_noun, nouns[top_noun]))
 
-out.write("Most used sentence structures\n")
-for struct in list(structures)[:10]:
-    out.write("{},{}\n".format(
-        struct, structures[struct]))
+    out.write("Most used sentence structures\n")
+    for struct in list(structures)[:10]:
+        out.write("{},{}\n".format(
+            struct, structures[struct]))
+
+@dataclass
+class TwitAnalysis:
+    totalTweets: int
+    totalSentences: int
+    avgSentenceLen: tuple[int, int]
+    mostUsedVerbs: dict[str, int]
+    mostUsedNouns: dict[str, int]
+    mostUsedStructs: dict[str, int]
+
+    def __init__(self) -> None:
+        self.mostUsedVerbs = {}
+        self.mostUsedNouns = {}
+        self.mostUsedStructs = {}
+
+
+def analysis():
+    '''
+    Run the analysis but do not write to a csv.
+    Return results as an object.
+    '''
+    print("Hang tight! Analyzing may take a while.")
+
+    # Load English tokenizer, tagger, parser and NER
+    with open('clean.txt', encoding="utf8") as file:
+        full_text = file.read()
+
+    # Boilerplate
+    sentences = getSentences(full_text)
+    nlp = spacy.load("en_core_web_sm")
+    # Set max length to avoid exception
+    doc = nlp(full_text[: 1000000])
+
+
+    # Analyze syntax (boilerplate)
+    noun_arr = [chunk.text for chunk in doc.noun_chunks]
+    verb_arr = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+
+    nouns = organizeNouns(noun_arr)
+    verbs = organizeVerbs(verb_arr)
+    structures = getStructures(sentences)
+
+    anal = TwitAnalysis()
+    anal.totalTweets = sum(1 for line in open('clean.txt', encoding="utf8"))
+    anal.totalSentences = len(sentences)
+    anal.avgSentenceLen = (aveSentLen(sentences), aveSentWords(sentences))
+
+    for top_verb in list(verbs)[:10]:
+        anal.mostUsedVerbs[top_verb] = verbs[top_verb]
+
+    for top_noun in list(verbs)[:10]:
+        anal.mostUsedNouns[top_noun] = verbs[top_noun]
+
+    for struct in list(structures)[:10]:
+        anal.mostUsedStructs[struct] = structures[struct]
+
+    return anal
